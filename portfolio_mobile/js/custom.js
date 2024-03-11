0659a485188a7ff54de757bc8adad69429372e41
jongju-Lee <jeje7@naver.com>
2024-03-12 00:07:29 +0900

    redefine cart complete

--- portfolio_mobile/js/custom.js
+++ portfolio_mobile/js/custom.js
@@ -12,6 +12,39 @@ $(function () {
       $("input[name=agree]").prop("checked", false);
     }
   });
+  /* ##### Cart - Check Function ##### */
+  $(".cart .check_all").click(function () {
+    // 장바구니에서 전체선택 클릭 했을때
+    $(this).toggleClass("active");
+    $(".cart .cart_item").toggleClass("active");
+  });
+  $(".cart .btn_all_clear").click(function () {
+    // 장바구니에서 선택삭제 버튼 클릭했을때
+    $(".cart .cart_item.active").remove();
+    $(".cart .check_all").removeClass("active");
+    const itemLength = $(".cart_item").length;
+    if (itemLength === 0) {
+      // 결제금액란 없어짐
+      $(".calculate_outer").remove();
+      $(".empty_message").show();
+    }
+  });
+  $(".cart .cart_item").click(function () {
+    // 카트아이템을 클릭했을때 체크
+    $(this).toggleClass("active");
+  });
+  $(".cart .btn_close_cart_item").click(function () {
+    $(this).parent().remove();
+    const itemLength = $(".cart_item").length;
+    if (itemLength === 0) {
+      // 카트아이템의 갯수가 0개가 되면 전체선택 체크 해제
+      $(".cart .check_all").removeClass("active");
+      // 결제금액란 없어짐
+      $(".calculate_outer").remove();
+      $(".empty_message").show();
+    }
+  });
+
   /* ########## Slick.js ########## */
   $(".product_slider").slick({
     infinite: true,
