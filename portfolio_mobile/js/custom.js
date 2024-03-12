85c79d8f962c21b1be6729a2ac838d4f678a758c
jongju-Lee <jeje7@naver.com>
2024-03-13 03:25:35 +0900

    redefine preloading complete:

--- portfolio_mobile/js/custom.js
+++ portfolio_mobile/js/custom.js
@@ -1,4 +1,9 @@
 $(function () {
+  /* Entire - Header Back Button */
+  $(".btn_back").click(function () {
+    // 헤더 뒤로가기 버튼 클릭시 뒤로가기
+    window.history.back();
+  });
   /* ##### GNB Include ##### */
   $("footer").load("../include/gnb.html");
   /* ##### Register - Terms CheckBox ##### */
@@ -29,9 +34,9 @@ $(function () {
       $(".empty_message").show();
     }
   });
-  $(".cart .cart_item").click(function () {
+  $(".cart .cart_item_thum").click(function () {
     // 카트아이템을 클릭했을때 체크
-    $(this).toggleClass("active");
+    $(this).parent().toggleClass("active");
   });
   $(".cart .btn_close_cart_item").click(function () {
     $(this).parent().remove();
@@ -48,6 +53,15 @@ $(function () {
   $(".details .heart").click(function () {
     $(this).toggleClass("active");
   });
+  /* ##### Details - Modal ##### */
+  $(".btn_add_cart").click(function () {
+    $(".details_modal_overlay").addClass("active");
+    $(".details_modal").addClass("active");
+  });
+  $(".details_modal_overlay, .btn_continue").click(function () {
+    $(".details_modal_overlay").removeClass("active");
+    $(".details_modal").removeClass("active");
+  });
   /* ##### Main - Tab Menu ##### */
   $(".tab_menu li a").click(function () {
     $(".tab_menu li a").removeClass("active");
@@ -63,8 +77,8 @@ $(function () {
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    // autoplay: true,
-    autoplaySpeed: 3000,
+    autoplay: true,
+    autoplaySpeed: 5000,
     dots: true,
   });
 });
