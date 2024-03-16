c4940e2edac0ec805aff20dd91a437e51a189d44
jongju-Lee <jeje7@naver.com>
2024-03-17 02:21:23 +0900

    practical coding building

--- portfolio_practical/other(prototype)/video_landing/custom.js
+++ portfolio_practical/other(prototype)/video_landing/custom.js
@@ -4,6 +4,9 @@ $(function () {
     $(this).toggleClass("active");
     $(".full_modal").stop().fadeToggle();
   });
+  $(".container").click(function () {
+    $(".full_modal").stop().fadeOut();
+  });
   /* ##### Audio Volume ##### */
   const $audio = document.querySelector("#my_audio");
   $audio.volume = 0.3; // Volume 0 ~ 1
